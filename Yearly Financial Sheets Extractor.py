3190d6beeb92bfeec81366c037dea73a536c5d6e
QuantumLead1 <167350406+QuantumLead1@users.noreply.github.com>
2024-04-22 19:55:51 -0400

    Financial Statement Extractor
    After you type in a stock ticker, the excel file will extract its yearly financial statements and it will give in an excel file

---
+++ Yearly Financial Sheets Extractor.py
@@ -0,0 +1,32 @@
+import yfinance as yf
+import xlwings as xl
+import pandas as pd 
+
+wb = xl.Book()
+ws = wb.sheets.active
+
+
+name = input("What stock?")
+
+ticker = yf.Ticker(name)
+
+
+             
+IS = wb.sheets[0]
+IS.name = "Income Statement"
+IS.range("A1").value =  ticker.income_stmt
+IS.autofit()
+
+
+BS = wb.sheets.add("Balance_Sheet")
+
+BS.range("A1").value = ticker.balance_sheet
+BS.autofit()
+
+CF = wb.sheets.add("Cash Flow")
+CF.range("A1").value = ticker.cash_flow
+CF.autofit()
+
+Info = wb.sheets.add("Stock Info")
+
+Info.api.OLEObjects("TextBox1").Object.Value = ticker.info
